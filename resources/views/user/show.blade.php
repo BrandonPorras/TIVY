4bf78f619aea216b7523ad8b25b438d3441872e0
BreandonPorras <brandon.porras.26@gmail.com>
2019-09-11 16:51:08 -0600

    logout

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -1,8 +1,43 @@
 @extends('layouts.app')
 
-@section('title', $user->name)
+@section('header')   
+    @component('components.header')
+    @endcomponent 
+@endsection
 
 @section('content')
-
- <h1>Hola si sirve</h1>
+    <div class="container-fluid ">
+        <div class="row d-flex ">
+            <div class="col-sm-2 col-12 bg-warning">
+                @component('components.card.profile',
+                ['img'=>'/storage/milos.png',
+                'name'=>'Ricardo Milos',
+                'description'=>'Bailar, sonreir, ser Ricardo Milos',
+                'email'=>'ricardomilos@dance.com',
+                'facebook'=>'Ricardo Milos',
+                'activities'=>'activity'
+                ])
+                @endcomponent
+            </div>
+            <div class="col-sm-5 col-12 bg-primary">
+                @component('components.activities',['activityTitle'=>'My Tivys'])
+                @endcomponent
+                @component('components.activities',['activityTitle'=>'Pending Tivys'])
+                @endcomponent
+            </div>
+            <div class="col-sm-5 col-12  bg-danger">
+                @component('components.activities',['activityTitle'=>'Upcoming Tivys'])
+                @endcomponent
+                @component('components.activities',['activityTitle'=>"Tivys I've gone to"])
+                @endcomponent
+            </div>
+        </div>
+    </div>
 @endsection
+
+@section('footer')
+    @component('components.footer')        
+    @endcomponent
+@endsection
+
+
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
 Route::get('/profile',function(){
     return view("pages.profile");}
 );
