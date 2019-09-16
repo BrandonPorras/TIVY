447a1bb29d04e015208b1be8451c0cc16ebf9290
BreandonPorras <brandon.porras.26@gmail.com>
2019-09-16 11:50:22 -0600

    lll

--- resources/views/components/hero.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="p-0 d-flex flex-column col-12 col-sm-8 justify-content-center">
-    <img src="/storage/tivy_hero.png" class="img-fluid" alt="Responsive image">
-    <div class="row position-absolute col-12 d-flex justify-content-center flex-column "> 
-        <h1 class="text-light mt-1 d-none d-sm-block" style="text-align:center">Welcome to</h1>
-        <img src="/storage/tivy_logo_blanco.svg" class="mt-1 text-light d-none d-sm-block" alt="Responsive image" style="height:10rem;"> 
-        <h2 class="text-light mt-1 d-none d-sm-block" style="text-align:center">Create, join Tivy's, make new friends</h2>
-        <h3 class="mt-1 text-light d-none d-sm-block" style="text-align:center">and experiences</h3>
-    </div>
-</div>
--- resources/views/components/link/activity.blade.php
+++
@@ -1,3 +0,0 @@
-<a href="{{$link}}" class="rounded-circle py-1" style="width:30%">
-    <img src={{$img}} alt="" class="img-fluid">
-</a>
