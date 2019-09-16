42c9cf8025b877186d96b07e27cc5817c6a3bf3b
BreandonPorras <brandon.porras.26@gmail.com>
2019-09-16 17:46:39 -0600

    se puede desloguear

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -37,9 +37,43 @@
         <li class="nav-item active">
           <a class="nav-link active text-dark" href="#">@lang('Profile')</a>
         </li>
-        <li class="nav-item active">
-          <a class="nav-link text-dark" href="#">@lang('Logout')</a>
+        @guest
+                      
+
+        <li class="nav-item navbar-dark">
+            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+        </li>                         
+        @if (Route::has('register'))
+            <li class="nav-item">
+                <a  class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+            </li>
+        @endif
+    @else    
+        <li class="nav-item dropdown">
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name }} <span class="caret"></span>
+            </a>
+
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+
+
+                 <a class="" href="{{ route('user.show', Auth::user()) }}">{{ Auth::user()->name }}  </a>
+                
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </div>  
         </li>
+
+       
+
+    @endguest
       </ul>
       </div>
     </div>
