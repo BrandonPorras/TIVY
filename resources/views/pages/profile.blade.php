7dd5397d0157dd07ed8126d4d4b32dda481350b8
BreandonPorras <brandon.porras.26@gmail.com>
2019-09-25 20:30:18 -0600

    filtros

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -7,10 +7,6 @@
 @endsection
 
 <!--Hice push-->
-<?php 
-use TIVY\tivy;    
-$tivies= Tivy::all();     
-?>
 
 @section('content')
     <div class="container-fluid ">
@@ -48,16 +44,16 @@ $tivies= Tivy::all();
                 </div>
             </div>
             <div class="col-lg-5 col-12 bg-white">
-                @component('components.activities',['activityTitle'=>'My Tivys','buttonCreate'=>true,'tivies'=>$tivies])
+                @component('components.activities',['activityTitle'=>'My Tivys','buttonCreate'=>true,'tivies'=>$myTivies])
               
                 @endcomponent
-                @component('components.activities',['activityTitle'=>'Pending Tivys','buttonCreate'=>false,'tivies'=>$tivies])
+                @component('components.activities',['activityTitle'=>'Pending Tivys','buttonCreate'=>false,'tivies'=>$pendingTivies])
                 @endcomponent
             </div>
             <div class="col-lg-5 col-12  bg-white">
-                @component('components.activities',['activityTitle'=>'Upcoming Tivys','buttonCreate'=>false,'tivies'=>$tivies])
+                @component('components.activities',['activityTitle'=>'Upcoming Tivys','buttonCreate'=>false,'tivies'=>$upComingTivies])
                 @endcomponent
-                @component('components.activities',['activityTitle'=>"Tivys I've gone to",'buttonCreate'=>false,'tivies'=>$tivies])
+                @component('components.activities',['activityTitle'=>"Tivys I've gone to",'buttonCreate'=>false,'tivies'=>$tiviesIGone])
                 @endcomponent
             </div>
         </div>
