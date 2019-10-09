66197da137b95bd0475eabff4c07de8e3019f59e
Randall Quesada <anakin1198@gmail.com>
2019-10-09 17:28:31 -0600

    3 commit

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', true),
+    'debug' => env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY','base64:kD/Od/f8wwjuuXD4sRixOqRcWBE//snY18LHl86oUSY='),
+    'key' => env('APP_KEY'),
 
     'cipher' => 'AES-256-CBC',
 
